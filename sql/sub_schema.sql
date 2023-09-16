3b7896f0986cab236ba8d9938032d6694dbcb0f9
seokijang <fledspar@naver.com>
2023-09-16 15:19:44 +0900

    sql changes

--- sql/sub_schema.sql
+++ sql/sub_schema.sql
@@ -23,6 +23,12 @@ CREATE TABLE attachment(
     FOREIGN KEY (file_post_id) REFERENCES posts(post_id)
 );
 
+CREATE TABLE admin(
+    admin_user_name VARCHAR(255),
+    authority VARCHAR(255)
+    FOREIGN KEY (admin_user_name) REFERENCES users(user_id)
+);
+
 -- CREATE TABLE report(
 -- 	report_post_id INT,
 --     report_comment_id INT,
@@ -39,9 +45,9 @@ CREATE TABLE post_report(
     FOREIGN KEY (report_user_name) REFERENCES users(user_name)
 );
 
-CREATE TABLE comment_report(
-    report_comment_id INT,
-    report_user_name VARCHAR(255),
-    FOREIGN KEY (report_comment_id) REFERENCES comments(comment_id),
-    FOREIGN KEY (report_user_name) REFERENCES users(user_name)
-);
+-- CREATE TABLE comment_report(
+--     report_comment_id INT,
+--     report_user_name VARCHAR(255),
+--     FOREIGN KEY (report_comment_id) REFERENCES comments(comment_id),
+--     FOREIGN KEY (report_user_name) REFERENCES users(user_name)
+-- );
